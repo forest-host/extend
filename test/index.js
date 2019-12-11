bf69d189c536186c5b329d49662992e37bc82a70
Johan Schuijt <johan@publicpixels.nl>
2019-12-11 12:46:09 +0100

    add some tests

--- test/index.js
+++ test/index.js
@@ -3,12 +3,49 @@ import { assert } from 'chai';
 import extend from '../src';
 
 describe('Extend', () => {
-	it('merges simple objects', () => {
+	it('Merges simple objects', () => {
 		let a = { a: 0 };
 		let b = { b: 1 };
 
 		let result = { a: 0, b: 1 };
 
 		assert.deepEqual(extend(a, b), result);
-	})
+	});
+
+	it('Replaces keys', () => {
+		let a = { a: 0 };
+		let b = { a: 1 };
+
+		let result = { a: 1 };
+
+		assert.deepEqual(extend(a, b), result);
+	});
+
+	it('Merges all objects passed', () => {
+		let a = { a: 0 };
+		let b = { b: 1 };
+		let c = { c: 2 };
+
+		let result = { a: 0, b: 1, c: 2 };
+
+		assert.deepEqual(extend(a, b, c), result);
+	});
+
+	it('Concats arrays', () => {
+		let a = { a: [0] };
+		let b = { a: [1] };
+
+		let result = { a: [0, 1] };
+
+		assert.deepEqual(extend(a, b), result);
+	});
+
+	it('Merges nested objects', () => {
+		let a = { a: { b: 1 } };
+		let b = { a: { a: 0 } };
+
+		let result = { a: { a: 0, b: 1 } };
+
+		assert.deepEqual(extend(a, b), result);
+	});
 })
